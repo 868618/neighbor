6bfddb5f8990635d737c7ec9d8346961b0ab41e7
kenny <879464294@qq.com>
2020-12-16 22:54:33 +0800

    图片切割一下

--- pages/single/index.js
+++ pages/single/index.js
@@ -1,8 +1,13 @@
 Page({
     data: {
-        isAddBg: false
-    },
-    load () {
-        this.setData({ isAddBg: true })
+        isAddBg: false,
+        src: 'https://linjubang.imrookie.cn/nb/image/2020/12/16/b490a61d7cb04762acf3bec43ebbf904.png',
+        list: [
+            'https://linjubang.imrookie.cn/nb/image/2020/12/16/ddbd68ab28a64f0abea9416f524077b6.png',
+            'https://linjubang.imrookie.cn/nb/image/2020/12/16/144ba7dd599d4f8aa7edde9162c996c8.png',
+            'https://linjubang.imrookie.cn/nb/image/2020/12/16/10c40e0db526403dbe4b7c2a07451ec8.png',
+            'https://linjubang.imrookie.cn/nb/image/2020/12/16/4a04672cf3794e569db794050df9c308.png',
+            'https://linjubang.imrookie.cn/nb/image/2020/12/16/b2cbd91118fb4f37b1e3c8aac1087d9f.png'
+        ]
     }
 });
